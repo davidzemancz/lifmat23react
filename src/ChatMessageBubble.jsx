067a0d044485288cc30e409cc207abd0a5a08e0b
krevetka159 <dita.chabicovska@gmail.com>
2023-10-24 23:19:47 +0200

    kod

--- src/ChatMessageBubble.jsx
+++ src/ChatMessageBubble.jsx
@@ -47,7 +47,7 @@ const MessageContent = ({message, setLoading, setUpdate}) => {
             <Link 
             sx={{ cursor: 'pointer' }}
             onClick={() => {
-              handleSend(opt.name)
+              handleSend(opt.name + ',' + opt.kod)
               }}>
                 {opt.name}
             </Link>
